330455234d1f9ae253a3d18db8eb3db1f8926fb5
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2022-10-24 22:47:23 +0200

    Vorrei bestemmiare

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-for(let i = 1; i <= 100; i++ ) {
-    if (i % 15 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
-    else console.log(i);
-    console.log(i)
+const outputHtml = document.querySelector('.container')
+for(let i=1 ; i<=100;i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        outputHtml.innerHTML += '<div class="box fizzbuzz">Fizzbuz</div>'
+    }
+    else if(i % 3 === 0){
+        outputHtml.innerHTML += '<div class="box fizz">Fizz</div>'
+    }
+    else if(i % 5 ===0){
+        outputHtml.innerHTML += '<div class="box buzz">Buzz</div>'
+    }
+    else{
+        outputHtml.innerHTML += `<div class="box"> ${i} </div>`
+    }
 }
